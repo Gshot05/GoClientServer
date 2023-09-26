74a96ad99d1cb04cc60d83ad1ba4d9e3c6d21986
Ислам <114616202+Gshot05@users.noreply.github.com>
2023-09-26 18:41:27 +0300

    Update Client.go

--- Client.go
+++ Client.go
@@ -1,12 +1,12 @@
 package main
 
 import (
-	"bytes"
 	"encoding/json"
-	"io/ioutil"
 	"log"
 	"net/http"
 	"time"
+    	"fmt"
+	"bytes"
 )
 
 type Display struct {
@@ -17,7 +17,7 @@ type Display struct {
 }
 
 type Monitor struct {
-	Voltage   float32 `json:"voltage"`
+	Voltage        float32 `json:"voltage"`
 	DisplayMonitor Display `json:"display"`
 	GSyncPrem      bool    `json:"gsync_prem"`
 	Curved         bool    `json:"curved"`
@@ -55,46 +55,66 @@ func addDisplay() {
 }
 
 func addMonitor() {
-	display := Display{
-		Diagonal:   27,
-		Resolution: "2560x1440",
-		TypeMatrix: "IPS",
-		GSync:      true,
-	}
-	monitor := Monitor{
-		Voltage:   220,
-		DisplayMonitor: display,
-		GSyncPrem:      true,
-		Curved:         false,
-	}
+    var display Display
+    fmt.Println("Введите данные для монитора:")
+    fmt.Print("Диагональ: ")
+    _, err := fmt.Scanf("%f", &display.Diagonal)
+    if err != nil {
+        log.Fatal(err)
+    }
 
-	data, err := json.Marshal(monitor)
-	if err != nil {
-		log.Fatal(err)
-	}
+    fmt.Print("Разрешение: ")
+    _, err = fmt.Scanf("%s", &display.Resolution)
+    if err != nil {
+        log.Fatal(err)
+    }
 
-	resp, err := http.Post("http://127.0.0.1:8080/addMonitor",
-		"application/json",
-		bytes.NewBuffer(data))
+    fmt.Print("Тип матрицы: ")
+    _, err = fmt.Scanf("%s", &display.TypeMatrix)
+    if err != nil {
+        log.Fatal(err)
+    }
 
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer resp.Body.Close()
+    fmt.Print("Поддержка GSync (true/false): ")
+    _, err = fmt.Scanf("%t", &display.GSync)
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    monitor := Monitor{
+        Voltage:        220,
+        DisplayMonitor: display,
+        GSyncPrem:      true,
+        Curved:         false,
+    }
+
+    data, err := json.Marshal(monitor)
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    resp, err := http.Post("http://127.0.0.1:8080/addMonitor",
+        "application/json",
+        bytes.NewBuffer(data))
+
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer resp.Body.Close()
 }
 
 func getAll() {
-	time.Sleep(1 * time.Second)
-	resp, err := http.Get("http://127.0.0.1:8080/getAll")
+    time.Sleep(1 * time.Second)
+    resp, err := http.Get("http://127.0.0.1:8080/getAll")
 
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer resp.Body.Close()
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer resp.Body.Close()
 
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		log.Fatal(err)
-	}
-	log.Println(string(body))
+    body, err := ioutil.ReadAll(resp.Body)
+    if err != nil {
+        log.Fatal(err)
+    }
+    log.Println(string(body))
 }
