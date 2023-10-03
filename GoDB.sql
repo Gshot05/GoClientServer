5d793bf6a79fe2f26013a7661ffa860c6a2c8558
Ислам <114616202+Gshot05@users.noreply.github.com>
2023-10-03 16:51:38 +0300

    Update GoDB.sql

--- GoDB.sql
+++ GoDB.sql
@@ -27,4 +27,14 @@ CREATE TABLE Type_Users(
 	Name_email text not null,
 	Name_Is_Admin boolean not null
 );
+INSERT INTO Type_Users (ID_Type_Users, 
+						Name_Username, 
+						Name_Password, 
+						Name_email, 
+						Name_Is_Admin)
+values (1, 
+		'Admin', 
+		'42b1c2497694ae761ba3f36ea19b735e1f6fb77949ad676b01a2758dfcf05285', 
+	    'email@mail.com', 
+	    true);
 select * from Type_Users;
