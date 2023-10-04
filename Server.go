012b3dfcc3f9ddd7110aaf414b218257df7957ef
Ислам <114616202+Gshot05@users.noreply.github.com>
2023-10-04 15:16:18 +0300

    Update Server.go

--- Server.go
+++ Server.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"crypto/sha256"
 	"database/sql"
+	"encoding/hex"
 	"encoding/json"
 	"fmt"
 	"log"
@@ -9,6 +11,15 @@ import (
 
 	_ "github.com/lib/pq"
 )
+
+type User struct {
+	ID             int    `json:"id"`
+	Username       string `json:"username"`
+	Password       string `json:"-"`
+	Email          string `json:"email"`
+	IsAdmin        bool   `json:"is_admin"`
+	HashedPassword string `json:"-"`
+}
 
 type Display struct {
 	Diagonal   float32 `json:"diag"`
@@ -18,12 +29,14 @@ type Display struct {
 }
 
 type Monitor struct {
-	Voltage         float32 `json:"voltage"`
+	VoltagePower    float32 `json:"voltage"`
 	DisplayMonitor  Display `json:"display"`
 	GSyncPrem       bool    `json:"gsync_prem"`
 	Curved          bool    `json:"curved"`
 	Type_Display_ID int     `json:"type_display_id"`
 }
+
+var mapOfUsers = make(map[string]User)
 
 var db *sql.DB
 
@@ -38,6 +51,8 @@ func main() {
 
 	fmt.Println("Запуск сервера...")
 
+	http.HandleFunc("/auth", authHandler)
+	http.HandleFunc("/register", registerHandler)
 	http.HandleFunc("/addDisplay", addDisplay)
 	http.HandleFunc("/addMonitor", addMonitor)
 	http.HandleFunc("/getAll", getAll)
@@ -45,9 +60,110 @@ func main() {
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
+func authHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodPost:
+		login(w, r)
+	case http.MethodGet:
+		w.Write([]byte("Форма авторизации"))
+	default:
+		w.WriteHeader(http.StatusMethodNotAllowed)
+	}
+}
+
+func registerHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodPost:
+		register(w, r)
+	case http.MethodGet:
+		w.Write([]byte("Форма регистрации"))
+	default:
+		w.WriteHeader(http.StatusMethodNotAllowed)
+	}
+}
+
+func register(w http.ResponseWriter, r *http.Request) {
+	var user User
+	err := json.NewDecoder(r.Body).Decode(&user)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	existingUser, err := getUserByUsername(user.Username)
+	if err == nil && existingUser.Username == user.Username {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("Пользователь с таким именем уже существует"))
+		return
+	}
+
+	user.HashedPassword = hashPassword(user.Password, user.Username)
+	_, err = db.Exec("INSERT INTO Type_Users (Name_Username, Name_Password, Name_email, Name_Is_Admin) VALUES ($1, $2, $3, $4)",
+		user.Username, user.HashedPassword, user.Email, user.IsAdmin)
+
+	if err != nil {
+		log.Println("Ошибка при добавлении пользователя в базу данных:", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+}
+
+func login(w http.ResponseWriter, r *http.Request) {
+	var user User
+	err := json.NewDecoder(r.Body).Decode(&user)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	dbUser, err := getUserByUsername(user.Username)
+	if err != nil || dbUser.HashedPassword != hashPassword(user.Password, user.Username) {
+		w.WriteHeader(http.StatusUnauthorized)
+		return
+	}
+
+	token := generateToken(user.Username, dbUser.ID)
+	mapOfUsers[token] = dbUser
+
+	w.Write([]byte(token))
+}
+
+func getUserByUsername(username string) (User, error) {
+	var user User
+	err := db.QueryRow("SELECT * FROM Type_Users WHERE Name_Username = $1", username).
+		Scan(&user.ID, &user.Username, &user.HashedPassword, &user.Email, &user.IsAdmin)
+
+	return user, err
+}
+
+func hashPassword(password, username string) string {
+	hasher := sha256.New()
+	hasher.Write([]byte(password + username))
+	return hex.EncodeToString(hasher.Sum(nil))
+}
+
+func generateToken(username string, userID int) string {
+	token := sha256.Sum256([]byte(fmt.Sprintf("%s%d", username, userID)))
+	return hex.EncodeToString(token[:])
+}
+
 func addDisplay(w http.ResponseWriter, r *http.Request) {
+	token := r.Header.Get("Authorization")
+	if !isAuthorized(token) {
+		w.WriteHeader(http.StatusUnauthorized)
+		return
+	}
+
+	user, err := getUserByToken(token)
+	if err != nil || !user.IsAdmin {
+		w.WriteHeader(http.StatusForbidden)
+		return
+	}
+
 	var display Display
-	err := json.NewDecoder(r.Body).Decode(&display)
+	err = json.NewDecoder(r.Body).Decode(&display)
 	if err != nil {
 		w.WriteHeader(http.StatusBadRequest)
 		return
@@ -65,15 +181,27 @@ func addDisplay(w http.ResponseWriter, r *http.Request) {
 }
 
 func addMonitor(w http.ResponseWriter, r *http.Request) {
+	token := r.Header.Get("Authorization")
+	if !isAuthorized(token) {
+		w.WriteHeader(http.StatusUnauthorized)
+		return
+	}
+
+	user, err := getUserByToken(token)
+	if err != nil || !user.IsAdmin {
+		w.WriteHeader(http.StatusForbidden)
+		return
+	}
+
 	var monitor Monitor
-	err := json.NewDecoder(r.Body).Decode(&monitor)
+	err = json.NewDecoder(r.Body).Decode(&monitor)
 	if err != nil {
 		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
 
 	_, err = db.Exec("INSERT INTO Type_Monitor (Name_Voltage, Name_Gsync_Prem, Name_Curved, Type_Display_ID) VALUES ($1, $2, $3, $4)",
-		monitor.Voltage, monitor.GSyncPrem, monitor.Curved, monitor.Type_Display_ID)
+		monitor.VoltagePower, monitor.GSyncPrem, monitor.Curved, monitor.Type_Display_ID)
 
 	if err != nil {
 		log.Println("Ошибка при добавлении в базу данных:", err)
@@ -85,22 +213,28 @@ func addMonitor(w http.ResponseWriter, r *http.Request) {
 }
 
 func getAll(w http.ResponseWriter, r *http.Request) {
+	token := r.Header.Get("Authorization")
+	if !isAuthorized(token) {
+		w.WriteHeader(http.StatusUnauthorized)
+		return
+	}
+
 	var monitors []Monitor
 	rows, err := db.Query(`
-        SELECT
-            m.Name_Voltage,
-            m.Name_Gsync_Prem, 
-            m.Name_Curved,
-            d.Name_Diagonal,
-            d.Name_Resolution,
-            d.Type_Type,
-            d.Type_Gsync,
-			m.Type_Display_ID
-        FROM
-            Type_Monitor AS m
-        INNER JOIN
-            Type_Display AS d ON m.Type_Display_ID = d.ID_Type_Display
-    `)
+			SELECT
+				m.Name_Voltage,
+				m.Name_Gsync_Prem, 
+				m.Name_Curved,
+				d.Name_Diagonal,
+				d.Name_Resolution,
+				d.Type_Type,
+				d.Type_Gsync,
+				m.Type_Display_ID
+			FROM
+				Type_Monitor AS m
+			INNER JOIN
+				Type_Display AS d ON m.Type_Display_ID = d.ID_Type_Display
+		`)
 
 	if err != nil {
 		log.Println("Ошибка при запросе данных из базы данных:", err)
@@ -113,7 +247,7 @@ func getAll(w http.ResponseWriter, r *http.Request) {
 		var monitor Monitor
 		var display Display
 		err := rows.Scan(
-			&monitor.Voltage,
+			&monitor.VoltagePower,
 			&monitor.GSyncPrem,
 			&monitor.Curved,
 			&display.Diagonal,
@@ -142,6 +276,12 @@ func getAll(w http.ResponseWriter, r *http.Request) {
 }
 
 func getMonitor(w http.ResponseWriter, r *http.Request) {
+	token := r.Header.Get("Authorization")
+	if !isAuthorized(token) {
+		w.WriteHeader(http.StatusUnauthorized)
+		return
+	}
+
 	id := r.FormValue("id")
 	var monitor Monitor
 	var display Display
@@ -162,7 +302,7 @@ func getMonitor(w http.ResponseWriter, r *http.Request) {
 		WHERE
 			m.Type_Display_ID = $1
     `, id).Scan(
-		&monitor.Voltage,
+		&monitor.VoltagePower,
 		&monitor.GSyncPrem,
 		&monitor.Curved,
 		&display.Diagonal,
@@ -182,4 +322,17 @@ func getMonitor(w http.ResponseWriter, r *http.Request) {
 
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(monitor)
+}
+
+func isAuthorized(token string) bool {
+	_, authorized := mapOfUsers[token]
+	return authorized
+}
+
+func getUserByToken(token string) (User, error) {
+	user, exists := mapOfUsers[token]
+	if !exists {
+		return User{}, fmt.Errorf("пользователь не найден")
+	}
+	return user, nil
 }
